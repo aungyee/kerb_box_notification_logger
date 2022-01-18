7486436d9c582a24d43c491cfcc8b744ffbbab2d
Aung Yee <lyndon.lso@gmail.com>
2022-01-18 16:11:04 +0000

    change google sheet name

--- App/googledrive.py
+++ App/googledrive.py
@@ -3,7 +3,7 @@ import gspread
 sa = gspread.service_account(filename='config/service_account.json')
 sh = sa.open('Kerb Box Parsed Notifications')
 
-wks = sh.worksheet("Sheet1")
+wks = sh.worksheet("kerbbox-notif")
 
 
 def append_row_to_google_sheets(message):
